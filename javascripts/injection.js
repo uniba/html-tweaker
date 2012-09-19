03f4b147875054eea028c4947f1e1689c7a25543
Jun Komatsu <jun@uniba.jp>
2012-09-19 12:44:12 +0900

    initial loading file

---
+++ javascripts/injection.js
@@ -0,0 +1,7 @@
+document.write('<script src="https://raw.github.com/uniba/html-tweaker/master/javascripts/vendor/jquery-1.7.2.min.js"></script>');
+document.write('<script src="https://raw.github.com/uniba/html-tweaker/master/javascripts/vendor/EventEmitter.min.js"></script>');
+document.write('<script src="https://raw.github.com/uniba/html-tweaker/master/javascripts/vendor/timeline/timeline.js"></script>');
+document.write('<script src="https://raw.github.com/uniba/html-tweaker/master/javascripts/vendor/modernizr/modernizr.js"></script>');
+document.write('<script src="https://raw.github.com/uniba/html-tweaker/master/javascripts/vendor/three.js/three.min.js"></script>');
+document.write('<script src="https://raw.github.com/uniba/html-tweaker/master/javascripts/vendor/DOMRendererExtensions/DOMRenderer.js"></script>');
+document.write('<script src="https://raw.github.com/uniba/html-tweaker/master/javascripts/Tweaker.js"></script>');
