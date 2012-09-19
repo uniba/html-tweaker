0937abe052983e6b1752ab07c9f4c8d394394aa7
Haruma Kikuchi <haruma@uniba.jp>
2012-09-19 09:30:21 +0900

    send dom elements CSS3DRenderer

--- javascripts/Tweaker.js
+++ javascripts/Tweaker.js
@@ -7,42 +7,99 @@
     , bgColorElm = []
     , positionElm = []
     , imageElm = []
-    , bgImageElm = [];
+    , bgImageElm = []
+    , threejsElm = []
+    , threejsObj = [];
+    
+  // Three.js
+  var camera
+    , scene
+    , renderer
+    , controls;
 
-  for (var i=0, maxi=elm.length; i<maxi; i+=1) {
-    if ($(elm[i]).css('position') === 'absolute') {
-      positionElm.push(elm[i]);
+  init();
+
+  function animate() {    
+    requestAnimationFrame( animate );
+
+    if (counter == 90) {
+      moveDomElemetToCss3d();
     }
+    
+    if (counter > 90) {
+      for (var i=0, maxi=threejsObj.length; i<maxi; i+=1) {
+        threejsObj[i].rotation.y += Math.random() * Math.PI / 60;
+      }
+    }
+    
+    if (counter > 450 && counter % 1 == 0) {
+      updateColor();
+    }
+
+    if (counter > 500 && counter % 1 == 0) {
+      updateAbsolutePosition();
+    }
+
+    if (counter > 590 && counter % 1 == 0) {
+      updateBgColor();
+    }
+
+    if (counter > 600 && counter % 90 == 0) {
+      updatePosition();
+    }
+    
+    if (counter > 610 && counter % 1 == 0) {
+      replaceImgSrc();
+    }
+
+    //controls.update();
+    renderer.render(scene, camera);
+
+    counter++;
+  }
+
+  function moveDomElemetToCss3d() {
+    for (var i=0, maxi=threejsElm.length; i<maxi; i+=1) {
+      //if ($(threejsElm[i]).css('-webkit-transform') === 'none') {
+        createCss3dObject(threejsElm[i]);
+        //i = maxi;
+      //}
+    }
+    
+    /*
+    for (var i=0, maxi=imageElm.length; i<maxi; i+=1) {
+      if ($(imageElm[i]).css('-webkit-transform') === 'none') {
+        createCss3dObject(imageElm[i]);
+        i = maxi;
+      }
+    }
+    
+    for (var i=0, maxi=bgImageElm.length; i<maxi; i+=1) {
+      if ($(bgImageElm[i]).css('-webkit-transform') === 'none') {
+        createCss3dObject(bgImageElm[i]);
+        i = maxi;
+      }
+    }
+    */
   }
   
-  for (var i=0, maxi=elm.length; i<maxi; i+=1) {
-    if ($(elm[i]).css('color')) {
-      colorElm.push(elm[i]);
-    }
+  function createCss3dObject(e) {
+		var element = e
+		  , object = new THREE.CSS3DObject(element);
+    			  
+		object.position.x = $(element).offset().left - window.innerWidth  / 2 + $(element).width()  / 2;
+		object.position.y = $(element).offset().top  - window.innerHeight / 2 + $(element).height() / 2;
+		object.position.z = 0;
+		object.rotation.x = 0; //Math.PI / 12 * i;
+		object.rotation.y = 0; //Math.PI / 12 * i;
+		object.rotation.z = 0; //Math.PI / 12 * i;
+		object.scale.x = 1.0;
+		object.scale.y = 1.0;
+		scene.add(object);
+		
+		threejsObj.push(object);
   }
 
-  for (var i=0, maxi=elm.length; i<maxi; i+=1) {
-    if ($(elm[i]).css('background-color')) {
-      bgColorElm.push(elm[i]);
-    }
-  }
-  
-  imageElm = $(elm).filter('img');
-  for (var i=0, maxi=imageElm.length; i<maxi; i+=1) {
-    if ($(imageElm[i]).attr('height') === undefined) {
-      $(imageElm[i]).attr('height', $(imageElm[i]).css('height'));
-    }
-    if ($(imageElm[i]).attr('width') === undefined) {
-      $(imageElm[i]).attr('width', $(imageElm[i]).css('width'));
-    }
-  }
-
-  for (var i=0, maxi=elm.length; i<maxi; i+=1) {
-    if ($(elm[i]).css('background-image').substr(0, 3) === 'url') {
-      bgImageElm.push(elm[i]);
-    }
-  }
-  
   function updatePosition() {
     for (var i=0, maxi=elm.length; i<maxi; i+=Math.round(Math.random() * 10)) {
       var mt = parseInt($(elm[i]).css('margin-left').replace('px', ''));
@@ -76,35 +133,6 @@
     $(bgImageElm[bgImageIndex]).css('background-image','url("http://www.cheshirecat.jp/images/noise.gif")');
   }
 
-  animate();
-
-  function animate() {    
-    requestAnimationFrame( animate );
-
-    if (counter > 90 && counter % 1 == 0) {
-      replaceImgSrc();
-    }
-    
-    if (counter > 450 && counter % 1 == 0) {
-      updateColor();
-    }
-
-    if (counter > 500 && counter % 1 == 0) {
-      updateAbsolutePosition();
-    }
-
-    if (counter > 590 && counter % 1 == 0) {
-      updateBgColor();
-    }
-
-    if (counter > 600 && counter % 90 == 0) {
-      updatePosition();
-    }    
-
-    
-    counter++;
-  }
-
   // @format (hex|rgb|null) : Format to return, default is integer
   function random_color(format) {
     var rint = Math.round(0xffffff * Math.random());
@@ -122,5 +150,76 @@
         break;
     }
   }
+
+	function init() {
+	
+  	// DOM Element Collection
+	  for (var i=0, maxi=elm.length; i<maxi; i+=1) {
+      if ($(elm[i]).css('position') === 'absolute') {
+        positionElm.push(elm[i]);
+      }
+    }
+    
+    for (var i=0, maxi=elm.length; i<maxi; i+=1) {
+      if ($(elm[i]).css('color')) {
+        colorElm.push(elm[i]);
+      }
+    }
+  
+    for (var i=0, maxi=elm.length; i<maxi; i+=1) {
+      if ($(elm[i]).css('background-color')) {
+        bgColorElm.push(elm[i]);
+      }
+    }
+    
+    imageElm = $(elm).filter('img');
+    for (var i=0, maxi=imageElm.length; i<maxi; i+=1) {
+      if ($(imageElm[i]).attr('height') === undefined) {
+        $(imageElm[i]).attr('height', $(imageElm[i]).css('height'));
+      }
+      if ($(imageElm[i]).attr('width') === undefined) {
+        $(imageElm[i]).attr('width', $(imageElm[i]).css('width'));
+      }
+    }
+  
+    for (var i=0, maxi=elm.length; i<maxi; i+=1) {
+      if ($(elm[i]).css('background-image').substr(0, 3) === 'url') {
+        bgImageElm.push(elm[i]);
+      }
+    }
+    
+    threejsElm = $(elm).filter('img');
+    
+    // Three.js
+		camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+		camera.position.set(0, 0, 600);
+
+		/*
+		controls = new THREE.TrackballControls(camera);
+
+		controls.rotateSpeed = 1.0;
+		controls.zoomSpeed = 1.2;
+		controls.panSpeed = 0.8;
+
+		controls.noZoom = true;
+		controls.noPan = true;
+
+		controls.staticMoving = false;
+		controls.dynamicDampingFactor = 0.3;
+
+		controls.keys = [ 65, 83, 68 ];
+		*/
+
+		scene = new THREE.Scene();
+
+		renderer = new THREE.CSS3DRenderer();
+		renderer.setSize(window.innerWidth, window.innerHeight);
+		renderer.domElement.style.position = 'absolute';
+		renderer.domElement.style.top = 0;
+		renderer.domElement.style.zIndex = 100000;
+		document.body.appendChild(renderer.domElement);
+		
+		animate();
+	}
 
 })();
